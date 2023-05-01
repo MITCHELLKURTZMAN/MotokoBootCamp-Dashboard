ae01729ad5e7691aa7b289e00c72e6ea77b277aa
Mitch <mitchellkurtzman.aiken@gmail.com>
2023-05-01 00:51:47 -0400

    Added a footer hero images

--- frontend/components/Footer/Footer.tsx
+++ frontend/components/Footer/Footer.tsx
@@ -2,11 +2,13 @@ import React from "react"
 import "./_footer.scss"
 import { images } from "../../constants/constants"
 import logo from "../../assets/images/dfinity.svg"
+import hero from "../../assets/images/MBC-background.png"
 
 const Footer: React.FC = () => {
   return (
     <footer>
-      <img src={logo} alt="Footer Logo" className="footer-logo" />
+      {/* <img src={logo} alt="Footer Logo" className="footer-logo" /> */}
+      <img src={hero} alt="Footer Logo" className="footer-hero" />
     </footer>
   )
 }
