e7dbafa7947186bbb814dce796c82482255a5237
Mitch <mitchellkurtzman.aiken@gmail.com>
2023-05-04 20:05:00 -0400

    Added slider to help ticket to keep focus on submissions

--- frontend/components/Submit/HelpTicket.tsx
+++ frontend/components/Submit/HelpTicket.tsx
@@ -1,5 +1,6 @@
-import { useUserStore } from "../../store/userStore"
 import React, { useState } from "react"
+import { useUserStore } from "../../store/userStore"
+import "./_helpTicket.scss"
 
 interface HelpTicketProps {}
 
@@ -8,6 +9,7 @@ const HelpTicket: React.FC<HelpTicketProps> = ({}) => {
   const [githubUrl, setGithubUrl] = useState<string>("")
   const [selectedDay, setSelectedDay] = useState<number>(1)
   const [canisterId, setCanisterId] = useState<string>("")
+  const [isFormVisible, setFormVisible] = useState<boolean>(false)
 
   const handleHelpTicketSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault()
@@ -24,51 +26,62 @@ const HelpTicket: React.FC<HelpTicketProps> = ({}) => {
   return (
     <div className="help-ticket">
       <h2 className="help-ticket__header">Submit Help Ticket</h2>
-      <form className="help-ticket__form" onSubmit={handleHelpTicketSubmit}>
-        <label htmlFor="issueDescription">Issue Description:</label>
-        <textarea
-          id="issueDescription"
-          value={issueDescription}
-          onChange={(e) => setIssueDescription(e.target.value)}
-          maxLength={250}
-          required
-        />
-        <label htmlFor="githubUrl">GitHub URL or Gist:</label>
+      <label className="switch">
         <input
-          type="url"
-          id="githubUrl"
-          value={githubUrl}
-          onChange={(e) => setGithubUrl(e.target.value)}
-          required
+          type="checkbox"
+          onChange={() => setFormVisible(!isFormVisible)}
         />
+        <span className="slider round"></span>
+      </label>
+      {isFormVisible && (
+        <form className="help-ticket__form" onSubmit={handleHelpTicketSubmit}>
+          <label htmlFor="issueDescription">Issue Description:</label>
+          <textarea
+            id="issueDescription"
+            value={issueDescription}
+            onChange={(e) => setIssueDescription(e.target.value)}
+            maxLength={250}
+            required
+          />
+          <label htmlFor="githubUrl">GitHub URL or Gist:</label>
+          <input
+            type="url"
+            id="githubUrl"
+            value={githubUrl}
+            onChange={(e) => setGithubUrl(e.target.value)}
+            required
+          />
 
-        <label htmlFor="canisterId">Canister ID:</label>
-        <input
-          type="text"
-          id="canisterId"
-          value={canisterId}
-          onChange={(e) => setCanisterId(e.target.value)}
-          required
-        />
+          <label htmlFor="canisterId">Canister ID:</label>
+          <input
+            type="text"
+            id="canisterId"
+            value={canisterId}
+            onChange={(e) => setCanisterId(e.target.value)}
+            required
+          />
 
-        <label htmlFor="day">Day:</label>
-        <select
-          id="day"
-          value={selectedDay}
-          onChange={(e) => setSelectedDay(e.target.value as unknown as number)}
-          required
-        >
-          <option value="1">Day 1</option>
-          <option value="2">Day 2</option>
-          <option value="3">Day 3</option>
-          <option value="4">Day 4</option>
-          <option value="5">Day 5</option>
-        </select>
+          <label htmlFor="day">Day:</label>
+          <select
+            id="day"
+            value={selectedDay}
+            onChange={(e) =>
+              setSelectedDay(e.target.value as unknown as number)
+            }
+            required
+          >
+            <option value="1">Day 1</option>
+            <option value="2">Day 2</option>
+            <option value="3">Day 3</option>
+            <option value="4">Day 4</option>
+            <option value="5">Day 5</option>
+          </select>
 
-        <button className="btn" type="submit">
-          Submit Help Ticket
-        </button>
-      </form>
+          <button className="btn" type="submit">
+            Submit Help Ticket
+          </button>
+        </form>
+      )}
     </div>
   )
 }
